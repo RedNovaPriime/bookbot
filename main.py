c7512e3c53ada10decb9b3c105de27479e569a0a
dmk10 <dmk10@my.fsu.edu>
2025-02-07 22:22:12 -0500

    Character Counter

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+
+def charcount(file_contents):
+
+    lower_file_contents = file_contents.lower()
+    char_count = {}
+
+    for char in lower_file_contents:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    return char_count
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)  # Display the contents of the file to the console
         words = file_contents.split()
         wordcount = len(words)
-        print(wordcount)
 
+        print(charcount(file_contents))
 main()
+
